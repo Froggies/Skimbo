b652343b102f16c2d5c8c82ccbc8c6abf1c69163
Audrey <novak.audrey@gmail.com>
2012-10-07 19:08:39 +0200

    integration of the columns test

--- public/app/scripts/controllers/columns.js
+++ public/app/scripts/controllers/columns.js
@@ -1,9 +1,63 @@
 'use strict';
 
 publicApp.controller('ColumnsCtrl', function($scope, $http) {
-  /*$http.get('ex/trello')
+  $http.get('http://127.0.0.1:9000/ex/trello')
   	.success(function(json) {
 	      $scope.data = json;
   	});
-	*/
+
 });
+
+
+function dragOver(target, ev)
+{
+	ev.preventDefault();
+}
+
+function dragStart(target,ev)
+{
+	target.style.opacity = "0.5";
+	ev.dataTransfer.setData("text/html", target.innerHTML);
+	ev.dataTransfer.setData("id", target.id);
+	ev.dataTransfer.effectAllowed = 'move';
+}
+
+function dragEnter(target, ev) {
+	target.style.backgroundColor = "red";
+	ev.preventDefault();
+}
+
+function dragLeave(target, ev) {
+	target.style.backgroundColor = "pink";
+	ev.preventDefault();
+}
+
+function dragEnd(target,ev)
+{
+	target.style.backgroundColor = "pink";
+	target.style.opacity = "1";
+}
+
+function drop(target,ev)
+{
+	ev.preventDefault();
+	var fromHtml=ev.dataTransfer.getData("text/html");
+	var id=ev.dataTransfer.getData("id");
+
+	var toHtml = target.innerHTML;
+
+	target.innerHTML = fromHtml;
+	document.getElementById(id).innerHTML = toHtml;
+
+	target.style.backgroundColor = "pink";
+	target.style.opacity = "1";
+	// angular.element(target).scope().$root.$eval();
+	var scope = angular.element(target).scope();
+    // update the model with a wrap in $apply(fn) which will refresh the view for us
+    // scope.$apply(function() {
+    // 	console.log("test");
+    //     scope.$eval(scope.data);
+    // 	console.log("test apres");
+    // }); 
+
+}
