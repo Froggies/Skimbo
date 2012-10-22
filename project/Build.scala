bc83f50658f7a31385ffdc7c419b852a772751d0
manland <rmaneschi@gmail.com>
2012-10-22 19:55:58 +0200

    fix bad config with mongoDb

--- project/Build.scala
+++ project/Build.scala
@@ -7,14 +7,6 @@ object ApplicationBuild extends Build {
     val appName         = "skimbo"
     val appVersion      = "0.1-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
-
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
-
 	val appDependencies = Seq(
     	"se.radley" %% "play-plugins-salat" % "1.1"
 	)
