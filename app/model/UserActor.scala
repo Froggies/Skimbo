9c3524a2d6970ec1daa1f0278e62f297d32c3c11
manland <rmaneschi@gmail.com>
2012-10-22 19:57:38 +0200

    transform User.scala in UserActor.scala

---
+++ app/model/UserActor.scala
@@ -0,0 +1,68 @@
+package model;
+
+import services.auth._
+
+import java.util.concurrent.TimeUnit
+
+import play.libs.{Akka => KK}
+import play.api._
+import play.api.Play.current
+import play.api.mvc._
+import play.api.libs._
+import play.api.libs.json._
+import play.api.libs.ws.{Response => ResponseWS, _}
+import play.api.libs.iteratee._
+import play.api.libs.concurrent._
+import play.api.libs.concurrent.execution.defaultContext
+
+import akka.actor._
+import akka.actor.ReceiveTimeout
+import akka.util._
+
+object User {
+  def create(out:Enumerator[String], request:RequestHeader, provider:GenericProvider, token:Any, endUrl:String):ActorRef = {
+    //new User(provider, token, endUrl);//not actor yet
+    //val system = ActorSystem("ActorSystem")
+    Akka.system.actorOf(Props(new User(out, request, provider, token, endUrl)), "myactor")
+  }
+}
+
+class User(out:Enumerator[String], r:RequestHeader, provider:GenericProvider, token:Any, endUrl:String) extends Actor {
+
+  private val interval = 5;//Config.getLong("mail.check.interval")
+  context.setReceiveTimeout(Duration(interval, TimeUnit.SECONDS))
+
+  def receive = {
+    case ReceiveTimeout => {
+      self ! Dispatch          
+    }
+    case Dispatch => dispatch()
+    case e: Exception => println("unexpected Error: " + e)
+  }
+
+  def dispatch() { 
+      implicit var request = r; 
+      sender ! provider.fetch(endUrl).get; 
+  }
+}
+
+case class Dispatch()
+
+class GenericPost {
+
+}
+
+object SseActor {
+
+  val toGenericPost: Enumeratee[JsValue, GenericPost] = Enumeratee.map[JsValue] {
+    case e@Operation(_, amout) if amout > lowerBound && amout < higherBound => e
+    case e@SystemStatus(_) => e
+  }
+
+  def operations(r:RequestHeader, provider:GenericProvider, endUrl:String): 
+    Enumerator[JsValue] = Enumerator.generateM[JsValue] {
+      //KK.asPromise(User.create(Enumerator(), r, provider, "", endUrl) ! Dispatch);
+      implicit var request = r; 
+      provider.fetch(endUrl).get.map{r => Some(r.json)};
+  }
+}
