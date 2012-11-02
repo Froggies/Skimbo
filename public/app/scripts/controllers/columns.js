43e65953314296486bc2542908b47c40f5d36898
manland <rmaneschi@gmail.com>
2012-11-02 20:01:21 +0100

    add WebSocket for client

--- public/app/scripts/controllers/columns.js
+++ public/app/scripts/controllers/columns.js
@@ -1,8 +1,41 @@
 'use strict';
 
 publicApp.controller('ColumnsCtrl', function($scope, $http) {
-  if (!!window.EventSource) {
-    var source = new EventSource('http://127.0.0.1:9000/api/stream/sse');
+  var wshost = 'ws://127.0.0.1:9000/api/stream/webSocket';
+  var ssehost = 'http://127.0.0.1:9000/api/stream/sse';
+  var sseping = 'http://127.0.0.1:9000/api/stream/ping';
+
+  if(window.MozWebSocket) {
+    window.WebSocket=window.MozWebSocket;
+  }
+  if(!window.WebSocket) {
+    //alert('Votre navigateur ne supporte pas les webSocket!');
+    return false;
+  } else {
+    var socket = new WebSocket(wshost);
+    socket.onopen = function() { console.log('socket ouverte'); }
+    socket.onclose = function() { console.log('socket fermée'); }
+    socket.onerror = function() { console.log('Une erreur est survenue'); }
+    socket.onmessage = function(msg){
+      var data;
+      try { //tente de parser data
+        data = JSON.parse(msg.data);
+      } catch(exception) {
+        data = msg.data
+      }      
+      //ici on poura effectuer tout ce que l'on veux sur notre objet data
+      console.log(data);
+      $scope.$apply(function() {
+        if($scope.data == undefined) {
+            $scope.data = [];
+          }
+        $scope.data.unshift(data);
+      });
+    }
+  } 
+
+  if (!window.WebSocket && !!window.EventSource) {
+    var source = new EventSource(ssehost);
     source.addEventListener('message', function(e) {
     	$scope.$apply(function() {
     		if($scope.data == undefined) {
@@ -11,7 +44,7 @@ publicApp.controller('ColumnsCtrl', function($scope, $http) {
     		$scope.data.unshift(JSON.parse(e.data));
     	});
       console.log("ping");
-      $http.get('http://127.0.0.1:9000/api/stream/ping');
+      $http.get(sseping);
     }, false);
 
     source.addEventListener('open', function(e) {
