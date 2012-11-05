89f0a9e0cff513c1a8df2c1c04f15b5377eeff50
Audrey <novak.audrey@gmail.com>
2012-11-05 23:04:20 +0100

    add angular controls to show or hide userInformations fix search bug

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -2,9 +2,11 @@
 
 publicApp.controller('MainCtrl', function($scope, $http) {
 
-  $scope.logout = function() {
-  	console.log("logout");
-    $http.get("http://127.0.0.1:9000/api/logout");
-  }
+	$scope.showUserInformations = false;
+
+	$scope.logout = function() {
+		console.log("logout");
+	    $http.get("http://127.0.0.1:9000/api/logout");
+	}
 
 });
