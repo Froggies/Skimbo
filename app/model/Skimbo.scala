69cb0183107b00d038000c9e79c0d6484029eae2
Julien Lafont <julien.lafont@studio-dev.fr>
2012-11-10 16:34:55 +0100

    Add 2 new properties to Skimbo
    DirectLink : link to ressource
    
    SinceId : Identifier used by "since" request

--- app/model/Skimbo.scala
+++ app/model/Skimbo.scala
@@ -7,11 +7,13 @@ import SocialNetwork._
 * Common format between social networks
 */
 case class Skimbo(
-	authorName: String,
-	authorScreenName: String,
-	message: String,
-	createdAt: DateTime,
-	comments: List[Skimbo],
-	from: SocialNetwork,
-	shared: Int
+  authorName: String,
+  authorScreenName: String,
+  message: String,
+  createdAt: DateTime,
+  comments: List[Skimbo],
+  from: SocialNetwork,
+  shared: Int,
+  directLink: String,
+  sinceId: String
 )
