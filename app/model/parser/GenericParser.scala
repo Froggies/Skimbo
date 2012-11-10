662550470f76a19dfdcaba3308c3fb54107c87df
Julien Lafont <julien.lafont@studio-dev.fr>
2012-11-10 16:05:34 +0100

    Add Generic Parser to Skimbo format

---
+++ app/model/parser/GenericParser.scala
@@ -0,0 +1,11 @@
+package model.parser
+
+import play.api.libs.json.Reads._
+import model.Skimbo
+import play.api.libs.json._
+
+trait GenericParser[T] {
+  def from(json: JsValue)(implicit fjs: Reads[T]): List[T] = json.as[List[T]]
+  def from(json: String)(implicit fjs: Reads[T]): List[T] = from(Json.parse(json))
+  def asSkimbos(elements: List[T]): List[Skimbo]
+}
