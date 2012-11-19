0d1cace11a6c5111231d49240136d48683220598
manland <rmaneschi@gmail.com>
2012-11-19 19:49:52 +0100

    add user infos from github

--- app/services/auth/providers/GitHub.scala
+++ app/services/auth/providers/GitHub.scala
@@ -3,6 +3,7 @@ package services.auth.providers
 import play.api._
 import play.api.mvc._
 import services.auth._
+import models.user.ProviderUser
 
 object GitHub extends OAuth2Provider {
 
@@ -14,4 +15,21 @@ object GitHub extends OAuth2Provider {
   override def processToken(response: play.api.libs.ws.Response) =
     Token((response.json \ "access_token").asOpt[String], None)
 
+  override def distantUserToSkimboUser(ident: String, response: play.api.libs.ws.Response): Option[ProviderUser] = {
+    try {
+      val me = response.json
+      val id = (me \ "id").as[Int].toString
+      val username = (me \ "login").asOpt[String]
+      val name = (me \ "name").asOpt[String]
+      val description = (me \ "bio").asOpt[String]
+      val profileImage = (me \ "avatar_url").asOpt[String]
+      Some(ProviderUser(id, this.name, username, name, description, profileImage))
+    } catch {
+      case _ => {
+        Logger.error("Error during fetching user details")
+        None
+      }
+    }
+  }
+    
 }
