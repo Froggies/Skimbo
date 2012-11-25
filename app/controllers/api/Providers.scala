23562e3528f2876923aa2adc4e131e6f5a2c02c0
manland <rmaneschi@gmail.com>
2012-11-25 12:26:46 +0100

    fix bug : multiple instanciation of ColumnCtrl = server side

--- app/controllers/api/Providers.scala
+++ app/controllers/api/Providers.scala
@@ -3,8 +3,10 @@ package controllers.api
 import play.api.mvc._
 import services.auth.ProviderDispatcher
 import models.Service
-import play.api.libs.json.Json
-import play.api.libs.json.JsArray
+import play.api.libs.json._
+import services.security.AuthenticatedAction.Authenticated
+import services.actors.ProviderActor
+import models.command.Command
 
 object Providers extends Controller {
 
@@ -14,6 +16,16 @@ object Providers extends Controller {
   
   def listServices = Action {  implicit req =>
     Ok(Service.toJsonWithUnifiedRequest).as(play.api.http.ContentTypes.JSON)
+  }
+  
+  def delete(providerName: String) = Authenticated { action =>
+    val providerOpt  = ProviderDispatcher(providerName);
+    providerOpt.map { provider =>
+      action.user.accounts.foreach { account =>
+        ProviderActor.killProfider(account.id, providerName)
+      }
+      Ok(Json.toJson(Command("deleteProvider", Some(JsString("ok"))))).as(play.api.http.ContentTypes.JSON);
+    }.getOrElse(BadRequest)
   }
 
 }
--- public/app/scripts/controllers/header.js
+++ public/app/scripts/controllers/header.js
@@ -3,8 +3,7 @@
 publicApp.controller('HeaderCtrl', function($scope, $http) {
 
 	$scope.deleteProvider = function(providerName) {
-      var cmd = JSON.stringify({"cmd":"deleteProvider", "body":{"provider": providerName}});
-      $http.post("/api/providers/delete", cmd).success(function() {
+      $http.get("/api/providers/del/"+providerName).success(function() {
         for (var i = 0; i < $scope.$parent.$parent.userInfos.length; i++) {
 	        if($scope.$parent.$parent.userInfos[i].socialType == providerName) {
 	          $scope.$parent.$parent.userInfos.splice(i,1);
