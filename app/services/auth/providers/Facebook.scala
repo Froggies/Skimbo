6392fd5b0b857582274b82bd17e8769a14cbe84e
manland <rmaneschi@gmail.com>
2012-11-26 01:32:09 +0100

    add facebook userInfos

--- app/services/auth/providers/Facebook.scala
+++ app/services/auth/providers/Facebook.scala
@@ -3,6 +3,8 @@ package services.auth.providers
 import play.api._
 import play.api.mvc._
 import services.auth._
+import models.user.ProviderUser
+import models.user.SkimboToken
 
 object Facebook extends OAuth2Provider {
 
@@ -18,6 +20,27 @@ object Facebook extends OAuth2Provider {
     response.body match {
       case AuthQueryStringParser(token, expires) => Token(Some(token), Some(expires.toInt))
       case _                                     => Token(None, None)
+    }
+  }
+  
+  override def distantUserToSkimboUser(ident: String, response: play.api.libs.ws.Response)(implicit request: RequestHeader): Option[ProviderUser] = {
+    try {
+      val me = response.json
+      val id = (me \ "id").as[String]
+      val name = (me \ "name").asOpt[String]
+      Some(ProviderUser(
+          id, 
+          this.name, 
+          Some(SkimboToken(getToken.get.token, None)), 
+          name, 
+          name, 
+          None, 
+          None))
+    } catch {
+      case _ : Throwable => {
+        Logger.error("Error during fetching user details LINKEDIN")
+        None
+      }
     }
   }
 
