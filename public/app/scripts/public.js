aea6e03e661f805e3fd059f79d6deafad65cea4d
Julien Lafont <julien.lafont@studio-dev.fr>
2012-11-30 12:41:20 +0100

    Hack to fix #53 : Redirect after FB auth

--- public/app/scripts/public.js
+++ public/app/scripts/public.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// HACK Facebook : redirect to #_=_ and angular dont like this ...
+if (document.location.hash == "#_=_") { document.location.replace("/"); }
+
 // Declare app level module which depends on filters, and services
 var publicApp = angular.module('publicApp', ['ngSanitize'], function($routeProvider, $locationProvider) {
   $routeProvider
