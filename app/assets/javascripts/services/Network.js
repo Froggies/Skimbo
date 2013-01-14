3e6b200948c88491eb7ed2409f95b57b8aecb420
manland <rmaneschi@gmail.com>
2013-01-14 22:15:37 +0100

    better network service for dotcloud and/or chrome

--- app/assets/javascripts/services/Network.js
+++ app/assets/javascripts/services/Network.js
@@ -2,52 +2,99 @@
 
 define(["app"], function(app) {
 
-app.factory("Network", ["$http", "ServerCommunication", function($http, $serverCommunication) {
+app.factory("Network", ["$http", "$timeout", "ServerCommunication", 
+  function($http, $timeout, $serverCommunication) {
 
   var wshost = jsRoutes.controllers.stream.WebSocket.connect().webSocketURL();
   var ssehost = jsRoutes.controllers.stream.Sse.connect().absoluteURL();
   var sseping = jsRoutes.controllers.stream.Sse.ping().absoluteURL();
 
+  var socket = undefined;//ws mode
+  var source = undefined;//sse mode
+  var nbError = 0;
+  var nbErrorMax = 10;
+  var dataReceivedByWS = false;
+
   function webSocketMode() {
-    socket = new WebSocket(wshost);
-    socket.onclose = function() { 
-      socket = undefined; 
-      sseMode(); 
-      console.log('WS socket fermée'); 
-    };
-    socket.onerror = function() { 
-      socket = undefined; 
-      sseMode(); 
-      console.log('WS Une erreur est survenue'); 
-    };
-    socket.onmessage = function(msg){
-      var data;
-      try {
-          data = JSON.parse(msg.data);
-      } catch(exception) {
-          data = msg.data;
-      }     
-      //console.log(data); 
-      command(data);
+    if(socket == undefined) {
+      socket = new WebSocket(wshost);
+      socket.onclose = function() { 
+        socket = undefined; 
+        nbError++;
+        $timeout(function() {
+          if(dataReceivedByWS == true && nbError < nbErrorMax) {
+            webSocketMode();
+          } else {
+            nbError = 0;
+            sseMode();
+          } 
+        }, 500);
+        console.log('WS closed : nbError = ', nbError); 
+      };
+      socket.onerror = function() { 
+        socket = undefined; 
+        nbError++;
+        $timeout(function() {
+          if(dataReceivedByWS == true && nbError < nbErrorMax) {
+            webSocketMode();
+          } else {
+            nbError = 0;
+            sseMode();
+          } 
+        }, 800);
+        console.log('WS error : nbError = ', nbError); 
+      };
+      socket.onmessage = function(msg){
+        dataReceivedByWS = true;
+        nbError = 0;
+        var data;
+        try {
+            data = JSON.parse(msg.data);
+        } catch(exception) {
+            data = msg.data;
+        }     
+        //console.log(data); 
+        command(data);
+      }
+      $timeout(function() {
+        if(dataReceivedByWS == false) {
+          console.log("no data after 5sc !");
+          //chrome doesn't fire error on proxy network !
+          socket = undefined;
+          nbError = 0;
+          sseMode();
+        }
+      }, 5000);
     }
   }
 
   function sseMode() {
-    var source = new EventSource(ssehost);
-    source.addEventListener('message', function(msg) {
-      var data;
-      try {
-          data = JSON.parse(msg.data);
-      } catch(exception) {
-          data = msg.data;
-      }      
-      command(data);
-    }, false);
+    console.log("sseMode actif !");
+    if(source == undefined) {
+      source = new EventSource(ssehost);
+      source.addEventListener('message', function(msg) {
+        nbError = 0;
+        var data;
+        try {
+            data = JSON.parse(msg.data);
+        } catch(exception) {
+            data = msg.data;
+        }      
+        command(data);
+      }, false);
 
-    source.addEventListener('error', function(e) {
-      source.close(); 
-      console.log('sse Une erreur est survenue');
-    }, false);
+      source.addEventListener('error', function(e) {
+        source.close(); 
+        source = undefined;
+        nbError++;
+        $timeout(function() {
+          if(nbError < nbErrorMax) {
+            sseMode();
+          } 
+        }, 800);
+        console.log('sse error : nb = ', nbError);
+      }, false);
+    }
   }
 
   function command(data) {
@@ -69,7 +116,6 @@ app.factory("Network", ["$http", "ServerCommunication", function($http, $serverC
     }
   }
   
-  var socket;
   if(window.MozWebSocket) {
     window.WebSocket = window.MozWebSocket;
   }
