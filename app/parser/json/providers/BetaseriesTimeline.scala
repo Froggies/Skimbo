c9aa9eed036b3ceb2f0206492c383efc62a68f1e
manland <rmaneschi@gmail.com>
2013-02-26 21:03:49 +0100

    better parse betaseries data

--- app/parser/json/providers/BetaseriesTimeline.scala
+++ app/parser/json/providers/BetaseriesTimeline.scala
@@ -11,6 +11,8 @@ import play.api.libs.json.Reads
 import play.api.libs.json.__
 import services.auth.providers.BetaSeries
 import parser.json.GenericJsonParser
+import play.api.libs.json.JsArray
+import play.api.libs.json.JsObject
 
 case class BetaseriesTimelineMessage(
   date: String,
@@ -49,31 +51,7 @@ object BetaseriesTimelineParser extends GenericJsonParser {
   }
 
   override def cut(json: JsValue) = {
-    val array =
-      JsArray(
-        Seq(
-          // TODO : smell not good, please help me to make better
-          json \ "root" \ "timeline" \ "0",
-          json \ "root" \ "timeline" \ "1",
-          json \ "root" \ "timeline" \ "2",
-          json \ "root" \ "timeline" \ "3",
-          json \ "root" \ "timeline" \ "4",
-          json \ "root" \ "timeline" \ "5",
-          json \ "root" \ "timeline" \ "6",
-          json \ "root" \ "timeline" \ "7",
-          json \ "root" \ "timeline" \ "8",
-          json \ "root" \ "timeline" \ "9",
-          json \ "root" \ "timeline" \ "10",
-          json \ "root" \ "timeline" \ "11",
-          json \ "root" \ "timeline" \ "12",
-          json \ "root" \ "timeline" \ "13",
-          json \ "root" \ "timeline" \ "14",
-          json \ "root" \ "timeline" \ "15",
-          json \ "root" \ "timeline" \ "16",
-          json \ "root" \ "timeline" \ "17",
-          json \ "root" \ "timeline" \ "18",
-          json \ "root" \ "timeline" \ "19"))
-
+    val array = JsArray((json \ "root" \ "timeline").asInstanceOf[JsObject].values.toSeq)
     super.cut(array)
   }
 
