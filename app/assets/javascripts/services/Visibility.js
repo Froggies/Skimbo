84522cad49ec93338fa73f45b5f9cb478178756e
manland <rmaneschi@gmail.com>
2013-03-11 11:29:44 +0100

    delete pagevisibility.js to integrate in angularJs service

--- app/assets/javascripts/services/Visibility.js
+++ app/assets/javascripts/services/Visibility.js
@@ -4,6 +4,40 @@ define(["app"], function(app) {
 
 app.factory("Visibility", function() {
 
+  var pageVisibility = (function(callWhenVisible, callWhenHidden) {
+    var hidden, change, vis = {
+            hidden: "visibilitychange",
+            mozHidden: "mozvisibilitychange",
+            webkitHidden: "webkitvisibilitychange",
+            msHidden: "msvisibilitychange",
+            oHidden: "ovisibilitychange"
+        };             
+    for (hidden in vis) {
+        if (vis.hasOwnProperty(hidden) && hidden in document) {
+            change = vis[hidden];
+            break;
+        }
+    }
+    if (change)
+        document.addEventListener(change, onchange);
+    else if (/*@cc_on!@*/false) // IE 9 and lower
+        document.onfocusin = document.onfocusout = onchange
+    else
+        window.onfocus = window.onblur = onchange;
+
+    function onchange (evt) {
+        var body = document.body;
+        evt = evt || window.event;
+
+        if (evt.type == "focus" || evt.type == "focusin")
+            callWhenVisible();
+        else if (evt.type == "blur" || evt.type == "focusout")
+            callWhenHidden();
+        else        
+            body.className = this[hidden] ? callWhenHidden() : callWhenVisible();
+    }
+  });
+
   var _isPageVisible = true;
   try {
     _isPageVisible = document.hasFocus();
