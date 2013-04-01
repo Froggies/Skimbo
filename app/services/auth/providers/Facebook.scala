c912a5546977c29f2e281be10ec76b3d0a085ba4
manland <rmaneschi@gmail.com>
2013-04-01 19:25:37 +0200

    remove logs

--- app/services/auth/providers/Facebook.scala
+++ app/services/auth/providers/Facebook.scala
@@ -39,7 +39,6 @@ object Facebook extends OAuth2Provider {
     val AuthQueryStringParser = """access_token=(.*)&expires=(.*)""".r
     val AuthQueryStringParser2 = """access_token=(.*)&expires=(.*)&auth_type=reauthenticate""".r
     val AuthQueryStringParser3 = """access_token=(.*)&auth_type=reauthenticate""".r
-    println("FACEBOOK token "+response.body.toString)
     response.body match {
       case AuthQueryStringParser(token, expires) => Token(Some(token), Some(expires.toInt), None)
       case AuthQueryStringParser2(token, expires)=> Token(Some(token), Some(expires.toInt), None)
