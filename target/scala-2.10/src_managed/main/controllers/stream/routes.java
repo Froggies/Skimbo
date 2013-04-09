a4f359e53fbeb28efe415e18f745fe470e9382bd
manland <rmaneschi@gmail.com>
2013-04-09 22:12:43 +0200

    deploy start on openshift

---
+++ target/scala-2.10/src_managed/main/controllers/stream/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/home/manland/projets/skimbo/Skimbo/conf/routes
+// @HASH:0db3a9eaaf9303eee8f28ddb45447bb6ccb672b4
+// @DATE:Tue Apr 09 22:10:40 CEST 2013
+
+package controllers.stream;
+
+public class routes {
+public static final controllers.stream.ReverseSse Sse = new controllers.stream.ReverseSse();
+public static final controllers.stream.ReverseWebSocket WebSocket = new controllers.stream.ReverseWebSocket();
+public static class javascript {
+public static final controllers.stream.javascript.ReverseSse Sse = new controllers.stream.javascript.ReverseSse();
+public static final controllers.stream.javascript.ReverseWebSocket WebSocket = new controllers.stream.javascript.ReverseWebSocket();    
+}   
+public static class ref {
+public static final controllers.stream.ref.ReverseSse Sse = new controllers.stream.ref.ReverseSse();
+public static final controllers.stream.ref.ReverseWebSocket WebSocket = new controllers.stream.ref.ReverseWebSocket();    
+} 
+}
+              
