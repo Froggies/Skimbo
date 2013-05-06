04f03b37339746489dfe24f468918b6d59cf34d9
manland <rmaneschi@gmail.com>
2013-05-06 19:05:32 +0200

    deploy start on openshift

---
+++ target/scala-2.10/src_managed/main/controllers/dev/routes.java
@@ -0,0 +1,16 @@
+// @SOURCE:/home/manland/projets/skimbo/Skimbo/conf/routes
+// @HASH:5994e1e51c0c55a11058a1e75ef1642bcd355445
+// @DATE:Mon May 06 19:03:07 CEST 2013
+
+package controllers.dev;
+
+public class routes {
+public static final controllers.dev.ReverseUtil Util = new controllers.dev.ReverseUtil();
+public static class javascript {
+public static final controllers.dev.javascript.ReverseUtil Util = new controllers.dev.javascript.ReverseUtil();    
+}   
+public static class ref {
+public static final controllers.dev.ref.ReverseUtil Util = new controllers.dev.ref.ReverseUtil();    
+} 
+}
+              
