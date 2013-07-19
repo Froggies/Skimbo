9aa34020880955908e5cc196cc0ccf5f9c6365a0
manland <rmaneschi@gmail.com>
2013-07-19 23:40:37 +0200

    remove old, unused line

--- app/assets/javascripts/controllers/MainController.js
+++ app/assets/javascripts/controllers/MainController.js
@@ -4,7 +4,6 @@ define(["app"], function(app) {
 
 app.controller('MainController', ['$scope', '$rootScope', function($scope, $rootScope) {
 
-	$scope.showUserInformations = false;
   // translation : 
   $rootScope.currentLanguage = navigator.language.substring(0,2);
 
