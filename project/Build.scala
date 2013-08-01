c606f07d47aba336f4f82052ff3885b82f11bc37
manland <rmaneschi@gmail.com>
2013-08-01 19:11:13 +0200

    nothing

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object ApplicationBuild extends Build {
 
   val appName         = "skimbo"
   val appVersion      = "0.1-SNAPSHOT"
-      
+
 	val appDependencies = Seq(
 	    "org.reactivemongo" %% "play2-reactivemongo" % "0.9",
 	    "org.apache.commons" % "commons-email" % "1.3.1"
