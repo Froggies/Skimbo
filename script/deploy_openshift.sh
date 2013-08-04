f83b4e2be3c39f8a8b22baf639e5f7ef8e3f59fb
manland <rmaneschi@gmail.com>
2013-08-04 21:37:14 +0200

    change play version into deploy openshift script

--- script/deploy_openshift.sh
+++ script/deploy_openshift.sh
@@ -4,7 +4,7 @@ REMOTE=5160195a5973cacccd0000bc@skimbo-froggies.rhcloud.com
 REMOTE_APP="~/diy-0.1/repo/"
 REMOTE_START="~/diy-0.1/repo/.openshift/action_hooks/start"
 REMOTE_STOP="~/diy-0.1/repo/.openshift/action_hooks/stop"
-LOCAL_PATH_PLAY="/home/manland/projets/play-2.1.1"
+LOCAL_PATH_PLAY="/home/manland/projets/play-2.1.2"
 LOCAL_PATH_PROJECT="/home/manland/projets/skimbo/Skimbo"
 
 cd $LOCAL_PATH_PROJECT
