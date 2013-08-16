fd0de0ba3ab4c7191eb54461e8ebfb79a124e927
manland <rmaneschi@gmail.com>
2013-08-16 21:57:37 +0200

    good url for new openshift architecture

--- script/deploy_openshift.sh
+++ script/deploy_openshift.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
  
 REMOTE=5160195a5973cacccd0000bc@skimbo-froggies.rhcloud.com
-REMOTE_APP="~/diy-0.1/repo/"
-REMOTE_START="~/diy-0.1/repo/.openshift/action_hooks/start"
-REMOTE_STOP="~/diy-0.1/repo/.openshift/action_hooks/stop"
+REMOTE_APP="~/app-root/repo/"
+REMOTE_START="~/app-root/repo/.openshift/action_hooks/start"
+REMOTE_STOP="~/app-root/repo/.openshift/action_hooks/stop"
 LOCAL_PATH_PLAY="/home/manland/projets/play-2.1.3"
 LOCAL_PATH_PROJECT="/home/manland/projets/skimbo/Skimbo"
 
