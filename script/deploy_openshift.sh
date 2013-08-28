cc2ad11a6042d7f1df5c2bda37cd4d8d05ed9207
manland <rmaneschi@gmail.com>
2013-08-28 20:32:32 +0200

    downgrade play to fix memory leak pb in prod

--- script/deploy_openshift.sh
+++ script/deploy_openshift.sh
@@ -4,7 +4,7 @@ REMOTE=5160195a5973cacccd0000bc@skimbo-froggies.rhcloud.com
 REMOTE_APP="~/app-root/repo/"
 REMOTE_START="~/app-root/repo/.openshift/action_hooks/start"
 REMOTE_STOP="~/app-root/repo/.openshift/action_hooks/stop"
-LOCAL_PATH_PLAY="/home/manland/projets/play-2.1.3"
+LOCAL_PATH_PLAY="/home/manland/projets/play-2.1.1"
 LOCAL_PATH_PROJECT="/home/manland/projets/skimbo/Skimbo"
 
 cd $LOCAL_PATH_PROJECT
